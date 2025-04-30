668b2e26d15ec9d7f5306a27cec6a20bd4dd90b7
ed system on behalf of Yao Ke 🥷 <ed>
2025-04-30 11:00:50 +0000

    Scaffold update by instructor

--- libs/document.h
+++ libs/document.h
@@ -1,4 +1,6 @@
 #ifndef DOCUMENT_H
+
+#define DOCUMENT_H
 /**
  * This file is the header file for all the document functions. You will be tested on the functions inside markdown.h
  * You are allowed to and encouraged multiple helper functions and data structures, and make your code as modular as possible. 
