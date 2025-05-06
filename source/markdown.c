be298e35561a16f947857515ce2ac1a79ace1991
ed system on behalf of Ryan Padamadan 🦥 <ed>
2025-05-06 01:25:58 +0000

    Scaffold update by instructor

---
+++ source/markdown.c
@@ -0,0 +1,90 @@
+#include "../libs/markdown.h"
+
+#define SUCCESS 0  // Return 0 for success as per your unit tests
+
+// === Init and Free ===
+document *markdown_init(void) {
+    return NULL;
+}
+
+void markdown_free(document *doc) {
+    (void)doc;
+}
+
+// === Edit Commands ===
+int markdown_insert(document *doc, uint64_t version, size_t pos, const char *content) {
+    (void)doc; (void)version; (void)pos; (void)content;
+    return SUCCESS;
+}
+
+int markdown_delete(document *doc, uint64_t version, size_t pos, size_t len) {
+    (void)doc; (void)version; (void)pos; (void)len;
+    return SUCCESS;
+}
+
+// === Formatting Commands ===
+int markdown_newline(document *doc, int version, int pos) {
+    (void)doc; (void)version; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_heading(document *doc, uint64_t version, int level, size_t pos) {
+    (void)doc; (void)version; (void)level; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_bold(document *doc, uint64_t version, size_t start, size_t end) {
+    (void)doc; (void)version; (void)start; (void)end;
+    return SUCCESS;
+}
+
+int markdown_italic(document *doc, uint64_t version, size_t start, size_t end) {
+    (void)doc; (void)version; (void)start; (void)end;
+    return SUCCESS;
+}
+
+int markdown_blockquote(document *doc, uint64_t version, size_t pos) {
+    (void)doc; (void)version; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_ordered_list(document *doc, uint64_t version, size_t pos) {
+    (void)doc; (void)version; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_unordered_list(document *doc, uint64_t version, size_t pos) {
+    (void)doc; (void)version; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_code(document *doc, uint64_t version, size_t start, size_t end) {
+    (void)doc; (void)version; (void)start; (void)end;
+    return SUCCESS;
+}
+
+int markdown_horizontal_rule(document *doc, uint64_t version, size_t pos) {
+    (void)doc; (void)version; (void)pos;
+    return SUCCESS;
+}
+
+int markdown_link(document *doc, uint64_t version, size_t start, size_t end, const char *url) {
+    (void)doc; (void)version; (void)start; (void)end; (void)url;
+    return SUCCESS;
+}
+
+// === Utilities ===
+void markdown_print(const document *doc, FILE *stream) {
+    (void)doc; (void)stream;
+}
+
+char *markdown_flatten(const document *doc) {
+    (void)doc;
+    return NULL;
+}
+
+// === Versioning ===
+void markdown_increment_version(document *doc) {
+    (void)doc;
+}
+
