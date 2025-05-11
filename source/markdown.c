6cdb456152bb18f80359be699e580de97290ef31
yimu xu <yixu0029@uni.sydney.edu.au>
2025-05-11 16:08:47 +1000

    test pipe creation and send sig

--- source/markdown.c
+++ source/markdown.c
@@ -1,19 +1,101 @@
 #include "../libs/markdown.h"
 
 #define SUCCESS 0 
+#define INVALID_CURSOR_POS -1
+#define DELETE_POSITION -2
+#define OUTDATE_VERSION -3
 
 // === Init and Free ===
 document *markdown_init(void) {
+    document *doc = malloc(sizeof(document));
+    doc->head->chunksize = 0;
+    doc->head->data = NULL;
+    doc->head->next = NULL;
+    doc->head->prev = NULL;
+    doc->head = malloc(sizeof(chunk));
+    doc->size = 0;
     return NULL;
 }
 
 void markdown_free(document *doc) {
-    (void)doc;
+    if (doc == NULL) {
+        return;
+    }
+    chunk *current = doc->head;
+    while (current != NULL) {
+        chunk *next = current->next;
+        free(current->data);
+        free(current);
+        current = next;
+    }
+    free(doc);
 }
 
+//hepler function split a chunk into two chunks
+chunk* split_chunk(chunk* current, size_t pos) {
+    chunk* new_chunk = malloc(sizeof(chunk));
+    new_chunk->data = malloc(current->chunksize - pos);
+    memcpy(new_chunk->data, current->data + pos, current->chunksize - pos);
+    new_chunk->chunksize = current->chunksize - pos;
+    new_chunk->next = current->next;
+    new_chunk->next->prev = new_chunk;
+    new_chunk->prev = current;
+    current->next = new_chunk;
+    current->chunksize = pos;
+    current->data = realloc(current->data, pos);
+    return current;
+}
 // === Edit Commands ===
 int markdown_insert(document *doc, uint64_t version, size_t pos, const char *content) {
-    (void)doc; (void)version; (void)pos; (void)content;
+    //(void)doc; (void)version; (void)pos; (void)content;
+    if(pos > doc->size) {
+        return INVALID_CURSOR_POS;
+    }
+    // check the version no sure if necessary
+    if(version != doc->head->chunksize) {
+        return OUTDATE_VERSION;
+    }
+    chunk* current = doc->head;
+    uint64_t current_pos = pos;
+    //find the chunk that contains the position
+    while (current != NULL && current_pos > current->chunksize) {
+        current_pos -= current->chunksize;
+        if(current_pos > 0 ) {
+            current = current->next;
+        }else {
+            break;
+        }         
+    }
+    // split the chunk if the position is in the middle of a chunk
+    if( current_pos > 0 && current != NULL) {
+            current = split_chunk(current, current_pos);
+            current_pos = 0;
+    }
+
+    if(current_pos == 0) {
+        if(current->next == NULL) {
+            chunk* new_chunk = malloc(sizeof(chunk));
+            new_chunk->data = content;
+            new_chunk->chunksize = strlen(content);
+            new_chunk->next = NULL;
+            // find last chunk
+            chunk* last_chunk = find_chunk(doc->head, pos);
+            new_chunk->prev = last_chunk;
+            last_chunk->next = new_chunk;
+            doc->size += new_chunk->chunksize;
+            return SUCCESS;
+        }else if(current->next != NULL) {
+            // create a new chunk
+            chunk* new_chunk = malloc(sizeof(chunk));
+            new_chunk->data = content;
+            new_chunk->chunksize = strlen(content);
+            new_chunk->next = current->next;
+            new_chunk->prev = current;
+            current->next->prev = new_chunk;
+            current->next = new_chunk;   
+            doc->size += new_chunk->chunksize;
+        }        
+    }
     return SUCCESS;
 }
 
--- source/server.c
+++ source/server.c
@@ -1,1 +1,136 @@
 // TODO: server code that manages the document and handles client instructions
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+
+int checkauthorisation(char* username, int c2sfd, int s2cfd){
+    // check if username is valid
+    FILE* fp = fopen("roles.txt", "r");
+    if(fp == NULL){
+        printf("open roles.txt error!\n");
+        return 1;
+    }
+    char line[256];
+    while(fgets(line, sizeof(line), fp)){
+        username[strcspn(username, "\n")] = 0; // remove  newline character username from client
+        //line[strcspn(line, "\n")] = 0; // remove newline character of file
+        char* token = strtok(line, " \t");
+        if(strcmp(token, username) == 0){
+            token = strtok(NULL, " \t");
+            printf("token: %s", token);/// for debug
+            write(s2cfd, token, strlen(token)+1);
+            fclose(fp);
+            return 0;
+        }
+
+    }
+    write(s2cfd,"Reject UNAUTHORISED\n", 21); //debug
+    printf("Reject UNAUTHORISED\n");
+    sleep(1);
+    fclose(fp);
+    return 1;
+
+}
+
+void* communication_thread(void* arg){
+    // create FIFO for communication
+    int* clientpid = (int*)arg;
+    char name1[] = "FIFO_C2S_";
+    char name2[] = "FIFO_S2C_";
+    char c2s[100];
+    char s2c[100];
+    sprintf(c2s, "%s%d",name1,*clientpid);
+    sprintf(s2c, "%s%d",name2,*clientpid);
+    unlink(c2s);
+    unlink(s2c);
+    mode_t perm = 0666;
+    
+    int rt1 = mkfifo(c2s, perm);
+    int rt2 = mkfifo(s2c, perm);
+    if(rt1 == -1){
+        printf("mkfifo error!\n");
+        return NULL;
+    }
+    if(rt2 == -1){
+        printf("mkfifo error!\n");
+        return NULL;
+    }else{
+        printf("FIFO_C2S created!\n");
+    }
+    // send signal to client
+    kill(*clientpid, SIGRTMIN+1);
+    // open FIFO
+    int c2sfd = open(c2s, O_RDONLY);
+    int s2cfd = open(s2c, O_WRONLY);
+    if(c2sfd == -1){
+        printf("open FIFO_C2S error!\n");
+        return NULL;
+    }
+    if(s2cfd == -1){
+        printf("open FIFO_S2C error!\n");
+        return NULL;
+    }
+    // read username from client
+    char username[256];
+    read(c2sfd, username, 256);
+    int rt = checkauthorisation(username, c2sfd, s2cfd);
+    if(rt == 1){
+        close(c2sfd);
+        close(s2cfd);
+        unlink(c2s);
+        unlink(s2c);
+        return NULL;
+    }
+    //loop for send command of editing
+    printf("success!\n");
+    close(c2sfd);
+    close(s2cfd);
+    unlink(c2s);
+    unlink(s2c);
+    return NULL;
+}
+
+
+int main(int argc, char** argv){
+    // check if user input time interval for update document
+    if(argc != 2){
+        printf("input time interval!\n");
+        return 1;
+    }
+    int serverpid = getpid();
+    printf("Server PID: %d\n", serverpid);
+    //////////////////////int time_interval = atoi(argv[1]);
+
+    sigset_t set;
+    siginfo_t info;
+    sigemptyset(&set);
+    sigaddset(&set, SIGRTMIN);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+    
+    // loop to accept the new client 
+    pthread_t thread;
+    int infopid = sigwaitinfo(&set, &info);
+    if (infopid == -1){
+        printf("sigwaitinfo error!\n");
+        return 1;
+    }
+    int clientpid = info.si_pid;
+    // initilaize thread
+    pthread_create(&thread, NULL, communication_thread, &clientpid);      
+    pthread_join(thread, NULL);  
+
+    
+
+    return 0;
+    
+}
