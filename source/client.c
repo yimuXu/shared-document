855bf48c6bbe0ec698756311ea61df8a30d2e202
yimu xu <yixu0029@uni.sydney.edu.au>
2025-05-23 11:11:45 +1000

    try put sleep in different loc

--- source/client.c
+++ source/client.c
@@ -116,20 +116,20 @@ void* receive_broadcast (void* arg){
         
         if (size > 0) {
             temp[size] ='\0';
-            // current_verison_log = malloc(size+1);
-            // strncpy(current_verison_log, temp, size);
-            // current_verison_log[size] = '\0';
+            current_verison_log = malloc(size+1);
+            strncpy(current_verison_log, temp, size);
+            current_verison_log[size] = '\0';
             //printf("receive:\n%ssize:%d\njiewei\n",temp,size);
             // lock  doc
             pthread_mutex_lock(&doc_mutex);
-            //edit_local_doc(current_verison_log);
-            edit_local_doc(temp);///////////////
+            edit_local_doc(current_verison_log);
+            //edit_local_doc(temp);///////////////
             markdown_increment_version(local_doc);
             pthread_mutex_unlock(&doc_mutex);
             //lock log
             pthread_mutex_lock(&log_mutex);
-           // append_log_to_all(current_verison_log);//////////
-           append_log_to_all(temp);
+            append_log_to_all(current_verison_log);//////////
+            //append_log_to_all(temp);
             pthread_mutex_unlock(&log_mutex);
             free(current_verison_log);
             //printf("update client doc\n");
